7b2a55c6019dab62e2ab157edc76a715df8e3e0a
Adrien Faure <adrien.faure@protonmail.com>
2025-12-05 11:41:33 +0100

    day-2: p2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,6 +1,6 @@
-use itertools::*;
 use std::io::Read;
 
+use itertools::Itertools;
 use std::{fs::File, io::BufReader};
 
 fn main() -> std::io::Result<()> {
@@ -30,5 +30,47 @@ fn main() -> std::io::Result<()> {
         .sum::<u64>();
 
     println!("p1: {}", p1);
+
+    let p2 = input
+        .trim()
+        .split(',')
+        .map(|e| {
+            e.split('-')
+                .map(|e| e.parse::<u64>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .flat_map(|(min, max)| {
+            (min..=max).filter_map(|n: u64| {
+                let mut nb_digit = n.ilog10() + 1;
+                if n == 0 {
+                    nb_digit = 1;
+                }
+
+                // println!("<<<<--- {n} --->>>>");
+                // 111 111 - 11 11 11  - 1 1 1 1 1 1
+                // 2         3           1
+                (1..nb_digit)
+                    .filter(|nd_i| nb_digit.is_multiple_of(*nd_i))
+                    .find_map(|n_split| {
+                        let mut sub_n = n;
+                        let n_times = nb_digit / n_split;
+
+                        if (0..n_times).map(|_| {
+                            let r = sub_n % 10_i32.pow(n_split) as u64;
+                            sub_n /= 10_i32.pow(n_split) as u64;
+                            // print!("{r}, ");
+                            r
+                        }).all_equal() {
+                            return Some(n);
+                        }
+
+                        None
+                    })
+            })
+        })
+        .sum::<u64>();
+
+    println!("p2: {}", p2);
     Ok(())
 }
