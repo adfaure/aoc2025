85456e5ed58711a40dbdb7fc8abe0c41d796e061
Adrien Faure <adrien.faure@protonmail.com>
2025-12-06 10:26:17 +0100

    day-3: p2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
 use std::io::BufRead;
-use std::io::Read;
 
 use std::{fs::File, io::BufReader};
 // 16456
@@ -24,14 +23,48 @@ fn main() -> std::io::Result<()> {
                 .skip(max_index + 1)
                 // .inspect(|e| println!("{e}"))
                 .map(|c| c.to_digit(10).unwrap())
-                .max().unwrap();
+                .max()
+                .unwrap();
 
-            let res = max * 10 + second;
-            res
-
-        }).sum::<u32>();
+            max * 10 + second
+        })
+        .sum::<u32>();
 
     println!("{p1:?}");
+
+    let p2 = BufReader::new(File::open("myinput")?)
+        .lines()
+        .map_while(|l| l.ok())
+        .map(|l| {
+            let chars = l
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .collect_vec();
+
+            let n = 12;
+            let mut res = 0;
+            let mut index = 0;
+            let mut len = l.len();
+
+            for i in (0..n).rev() {
+                let max = chars.iter().skip(index).take(len - i).max().unwrap();
+
+                index = chars
+                    .iter()
+                    .enumerate()
+                    .skip(index)
+                    .find(|(_idx, x)| *x == max)
+                    .unwrap().0 + 1;
+
+                len = l.len() - index;
+
+                res += *max * 10_u64.pow(i as u32);
+            }
+            res
+        })
+        .sum::<u64>();
+
+    println!("{p2:?}");
 
     Ok(())
 }
