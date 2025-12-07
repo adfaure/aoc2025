2c1f37fd4b5a3b3eb17ad50a8418af13e42e5a13
Adrien Faure <adrien.faure@protonmail.com>
2025-12-07 15:39:25 +0100

    day-5: p1

---
+++ day-5/src/main.rs
@@ -0,0 +1,29 @@
+use itertools::Itertools;
+use std::io::BufRead;
+
+use std::{fs::File, io::BufReader};
+fn main() -> std::io::Result<()> {
+    let input_file = "myinput";
+    let ranges = BufReader::new(File::open(input_file)?)
+        .lines()
+        .map_while(|l| l.ok())
+        .take_while(|l| !l.is_empty())
+        .map(|r: String| {
+            let (start, end) = r.split_once('-').unwrap();
+            start.parse::<i64>().unwrap()..=end.parse::<i64>().unwrap()
+        })
+        .collect_vec();
+
+    let p1 = BufReader::new(File::open(input_file)?)
+        .lines()
+        .map_while(|l| l.ok())
+        .skip_while(|l| !l.is_empty())
+        .skip(1)
+        .map(|id| id.parse::<i64>().unwrap())
+        .filter(|id| ranges.iter().find(|r| r.contains(id)).is_some())
+        .count();
+
+    println!("{p1}");
+
+    Ok(())
+}
