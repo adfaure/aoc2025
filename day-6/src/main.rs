37429a5569e1b431b02760295e2191a47bd0cd58
Adrien Faure <adrien.faure@protonmail.com>
2025-12-07 18:36:02 +0100

    day-6: p2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -37,5 +37,47 @@ fn main() -> std::io::Result<()> {
 
     println!("p1: {p1:?}");
 
+    let matrix = pb
+        .iter()
+        .take(pb.len() - 1)
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+
+    let mut transposed: Vec<Vec<char>> = vec![vec!['.'; matrix.len()]; matrix[0].len()];
+
+    for j in 0..matrix.len() {
+        for i in 0..matrix[j].len() {
+            transposed[i][j] = matrix[j][i];
+        }
+    }
+
+    let p2 = transposed
+        .iter()
+        .map(|line| line.iter().filter(|c| **c != ' ').collect::<String>())
+        .batching(|it| match it.next() {
+            None => None,
+            Some(c) => {
+                let mut res = vec![c.parse::<i64>().unwrap()];
+                for c in it.by_ref() {
+                    if c.is_empty() {
+                        break;
+                    }
+                    res.push(c.parse::<i64>().unwrap());
+                }
+                Some(res)
+            }
+        })
+        .enumerate()
+        .map(|(op, numbers)| {
+            numbers.into_iter().reduce(|acc, n| match operations[op] {
+                "*" => n * acc,
+                "+" => n + acc,
+                _ => unreachable!(),
+            }).unwrap()
+        })
+        .sum::<i64>();
+
+    println!("p2: {p2}");
+
     Ok(())
 }
