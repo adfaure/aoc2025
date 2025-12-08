7ed05f840b842838349e457178ee6199cb6a0340
Adrien Faure <adrien.faure@protonmail.com>
2025-12-08 23:16:11 +0100

    day-8: ok

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,19 +1,19 @@
 use itertools::Itertools;
-use std::cell::RefCell;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::io::BufRead;
 use std::iter::once;
-use std::rc::Rc;
 use std::{fs::File, io::BufReader};
 
 fn dist3d(a: &(i64, i64, i64), b: &(i64, i64, i64)) -> i64 {
-    ((a.0 - b.0).abs().pow(2) + (a.1 - b.1).abs().pow(2) + (a.2 - b.2).abs().pow(2)).isqrt()
+    // No sqrt to gain time
+    (a.0 - b.0).abs().pow(2) + (a.1 - b.1).abs().pow(2) + (a.2 - b.2).abs().pow(2)
 }
 
 type Classes = Vec<HashSet<(i64, i64, i64)>>;
 
 fn main() -> std::io::Result<()> {
-    let input_file = "input";
+    let input_file = "myinput";
+
     let junction_boxes = BufReader::new(File::open(input_file)?)
         .lines()
         .map_while(|l| l.ok())
@@ -24,68 +24,80 @@ fn main() -> std::io::Result<()> {
         })
         .collect_vec();
 
-    let total = 10;
-
+    // Create a vector containing all the start classes
     let mut classes: Classes = junction_boxes
         .clone()
         .into_iter()
         .map(|a| HashSet::from([a]))
         .collect();
-    for _ in 0..total {
-        let min_dist = classes
+
+    // create all permutation
+    for (idx, min_dist) in junction_boxes
+        .clone()
+        .into_iter()
+        .combinations(2)
+        .map(|cl1| (cl1.clone(), dist3d(&cl1[0], &cl1[1])))
+        .sorted_by(|a, b| a.1.cmp(&b.1))
+        .map(|(c, _)| c)
+        .enumerate()
+    {
+
+        // Find the two classes associated to the
+        // boxes and merge them
+        let left = classes
             .iter()
             .enumerate()
-            .permutations(2)
-            .min_by(|cl1, cl2| {
-                let a = cl1[0]
-                    .1
-                    .iter()
-                    .cartesian_product(cl1[1].1.iter())
-                    .map(|e| (e, dist3d(e.0, e.1)))
-                    .min_by(|a, b| a.1.cmp(&b.1))
-                    .unwrap();
-
-                let b = cl2[0]
-                    .1
-                    .iter()
-                    .cartesian_product(cl2[1].1.iter())
-                    .map(|e| (e, dist3d(e.0, e.1)))
-                    .min_by(|a, b| a.1.cmp(&b.1))
-                    .unwrap();
-                a.1.cmp(&b.1)
-            })
+            .find(|(_, class)| class.contains(&min_dist[0]))
             .unwrap();
 
-        let left: HashSet<_> = min_dist[0].1.clone();
-        let mut new_class: HashSet<_> = min_dist[1].1.clone();
-        new_class.extend(&mut left.iter());
+        let mut new_class = classes
+            .clone()
+            .into_iter()
+            .enumerate()
+            .find(|(_, class)| class.contains(&min_dist[1]))
+            .unwrap();
+
+        new_class.1.extend(&mut left.1.iter());
 
         classes = classes
             .clone()
             .into_iter()
             .enumerate()
-            .filter(|(idx, _)| *idx != min_dist[0].0 && *idx != min_dist[1].0)
+            .filter(|(idx, _)| *idx != left.0 && *idx != new_class.0)
             .map(|(_, item)| item)
-            .chain(once(new_class))
+            .chain(once(new_class.1.clone()))
             .collect_vec();
 
-        for c in &classes {
-            println!("{:?} - len {}", c, c.len());
+        // go up to ten for example
+        if idx - 1 == 10 {
+            let p1 = classes
+                .iter()
+                .map(|class| class.len())
+                .sorted()
+                .rev()
+                .take(3)
+                .product::<usize>();
+
+            println!("example input: {}", p1);
         }
 
-        println!("------");
+        if idx - 1 == 1_000 {
+            let p1 = classes
+                .iter()
+                .map(|class| class.len())
+                .sorted()
+                .rev()
+                .take(3)
+                .product::<usize>();
 
+            println!("p1: {}", p1);
+        }
+
+        if classes.len() == 1 {
+            println!("p2: {:?}", min_dist[0].0 * min_dist[1].0);
+            break;
+        }
     }
-
-    let p1 = classes
-        .iter()
-        .map(|class| class.len())
-        .sorted()
-        .rev()
-        .take(3)
-        .product::<usize>();
-
-    println!("p1: {}", p1);
 
     Ok(())
 }
