d9d52754486d5fb4b8cb34b55828bbe5a7088d30
Adrien Faure <adrien.faure@protonmail.com>
2025-12-10 15:53:36 +0100

    day-10: p1

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+use std::collections::HashSet;
 use std::io::BufRead;
 use std::{fs::File, io::BufReader};
 
@@ -38,15 +40,56 @@ fn parse_line(s: String) -> (Vec<char>, Vec<Vec<i32>>, Vec<i32>) {
 }
 
 fn main() -> std::io::Result<()> {
-    let input_file = "input";
+    let input_file = "myinput";
 
     let p1 = BufReader::new(File::open(input_file)?)
         .lines()
         .map_while(|l| l.ok())
         .map(parse_line)
-        .collect_vec();
+        .map(|(indicators, buttons, _)| solve_p1(&indicators, &buttons))
+        .sum::<i32>();
 
     println!("p1: {:?}", p1);
 
     Ok(())
+}
+
+fn solve_p1(desired_state: &[char], buttons: &[Vec<i32>]) -> i32 {
+    let start = vec!['.'; desired_state.len()];
+    let mut seen = HashSet::new();
+
+    let mut deq = VecDeque::from(
+        (0..buttons.len())
+            .map(|button| (0, button, start.clone()))
+            .collect_vec(),
+    );
+
+    while let Some((idx, button_idx, indicators)) = deq.pop_front() {
+        if idx == 100 {
+            // Some bound so that I don't crash my laptop again
+            panic!()
+        }
+
+        if seen.insert((button_idx, indicators.clone())) {
+            if indicators == desired_state {
+                return idx
+            }
+
+            let mut pushed = indicators.clone();
+
+            buttons[button_idx].iter().for_each(|idx| {
+                if pushed[*idx as usize] == '.' {
+                    pushed[*idx as usize] = '#';
+                } else {
+                    pushed[*idx as usize] = '.';
+                }
+            });
+
+            for button in 0..buttons.len() {
+                deq.push_back((idx + 1, button, pushed.clone()));
+            }
+        }
+    }
+
+    unreachable!()
 }
